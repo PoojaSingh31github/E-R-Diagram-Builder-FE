5d7c3cfaea279a84a3c0b1a8d21070e745fa5db0
PoojaSingh31github <pooja.singh.masaischool@gmail.com>
2024-12-19 11:48:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './pages/home'
-import AllRoutes from './pages/AllRoutes'
-import RegisterForm from './pages/Tryresgister'
-import LoginForm from './pages/Trylogin'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <LoginForm/>
-    <RegisterForm/>
     </>
 
   )
